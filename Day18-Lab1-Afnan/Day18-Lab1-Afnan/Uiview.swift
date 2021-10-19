74b872e0a471e2e77efa45c26c1029470f44337f
Afnan khalid <Afnanmoghram@gmail.com>
2021-10-20 01:18:03 +0300

    done

---
+++ Day18-Lab1-Afnan/Day18-Lab1-Afnan/Uiview.swift
@@ -0,0 +1,21 @@
+//
+//  Uiview.swift
+//  Day18-Lab1-Afnan
+//
+//  Created by Fno Khalid on 13/03/1443 AH.
+//
+
+import UIKit
+
+//extension ViewController {
+//    
+//      func pin(to superview: ViewController) {
+//        translatesAutoresizingMaskIntoConstraints = false
+//        topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+//        leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
+//          tra
+//        
+//        
+//        
+//    }
+//}
