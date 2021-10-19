7111fc0998722ae98f0c4615e7bf92d7357bf1e7
dmdm <deemamoh15@hotmail.com>
2021-10-19 23:57:33 +0300

    done

---
+++ day18/day18/UIView.swift
@@ -0,0 +1,18 @@
+//
+//  UIView.swift
+//  day18
+//
+//  Created by dmdm on 19/10/2021.
+//
+
+import UIKit
+ 
+extension UIView {
+func pin (to superView: UIView) {
+     translatesAutoresizingMaskIntoConstraints = false
+     topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+     leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+     trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+     bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+}
+}
